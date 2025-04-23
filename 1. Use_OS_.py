ac0c32769084f410c5aaaa0600af53405a0da553
4Raisan <mn4raisan@gmail.com>
2025-04-23 23:21:01 +0530

    Update 1. Use_OS_.py

--- 1. Use_OS_.py
+++ 1. Use_OS_.py
@@ -1,1 +1,6 @@
+''' Can be using to recognize the other files on the same(script what we ran) or specific directory/location...
+# Script Location  # how to fined the files location
 
+- os.path.dirname(os.path.abspath(__file__)) : Get the directory where the Python script is located.
+- __file__ : Represents the relative path of the currently executing script. Use it with os.path functions to get the script's location.
+- os.listdir(os.path.dirname(os.path.abspath(__file__))) : to make the list of the other iteams on the same directory. '''
