97f7c73fdd7bd51cb20e67b9691b3137e5114ca9
4Raisan <mn4raisan@gmail.com>
2025-05-10 22:45:57 +0530

    add key note to explain some main points, before practice the complex codes

--- 3. Use_subprocess_Module.py
+++ 3. Use_subprocess_Module.py
@@ -1,4 +1,8 @@
 '''  .subprocess()  - Run and manage, System commands (CMD/PowerShell) or External programs.  '''
+# --- Key Notes 
+# List format preferred → subprocess.run(["echo", "Hello"])  → replace spaces with comma ( , )
+# Use raw strings (r"") for Windows paths
+#  shell=True  → only for Batch Script File Run or Command Run (for Raw - subprocess.run("echo %PATH%") )
 
 # Ping URLs
 import subprocess
@@ -10,5 +14,8 @@ subprocess.run(["notepad"])
 
 # Run Terminals (CMD/PowerShell)
 import subprocess
-subprocess.run(["cmd", "/c", "dir /w"])  # CMD
+subprocess.run(["cmd", "/c", "dir /w"])  # CMD  → ( /w - just names ) 
 subprocess.run(["powershell", "ls"])  # PowerShell
+
+
+
