0f6ed8bfb695eebeae1d3d4e274dfd39f970f11b
4Raisan <mn4raisan@gmail.com>
2025-05-11 00:23:43 +0530

    add more features like, open spedific paths/ files

--- 3. Use_subprocess_Module.py
+++ 3. Use_subprocess_Module.py
@@ -1,18 +1,29 @@
-'''  .subprocess()  - Run and manage, System commands (CMD/PowerShell) or External programs.  '''
+'''  .subprocess()  - Run and manage, System commands (CMD/PowerShell) or External programs.
+
 # --- Key Notes 
 # List format preferred → subprocess.run(["echo", "Hello"])  → replace spaces with comma ( , )
 # Use raw strings (r"") for Windows paths
-#  shell=True  → only for Batch Script File Run or Command Run (for Raw - subprocess.run("echo %PATH%") )
-#  capture_output=True  → capture the output of the teminal
-#  text=True  → convert the above capture into Python String format
 
-# Ping URLs
-import subprocess
-subprocess.run(["ping", "google.com"])
+      shell=True      → only for Batch Script File Run or Command Run (for Raw - subprocess.run("echo %PATH%") )
+  capture_output=True  → capture the output of the teminal
+      text=True      → convert the above capture into Python String format
 
-# Open Programs (Softwares)
+  stdout (Standard Output): Where a program sends its normal output (e.g., print statements).
+  stderr (Standard Error): Where a program sends error messages (e.g., warnings, exceptions).
+'''
+
+# Open or Ping URLs
 import subprocess
-subprocess.run(["notepad"])
+subprocess.run(["start", "https://google.com"], shell=True)  # Open URL
+subprocess.run(["ping", "google.com"])  # ping URL
+
+
+# Open Programs or Files (Softwares)
+import subprocess
+subprocess.run(["notepad", r"C:\User\Desktop\file.txt"])  # Open specific file
+subprocess.run(["explorer", r"C:\Windows"])  # Open File Explorer
+subprocess.run(["notepad"])  # open program
+
 
 # Run Terminals (CMD/PowerShell)
 import subprocess
@@ -26,7 +37,17 @@ subprocess.run(["powershell", "ls"])  # PowerShell
 # run in CMD shell / capture the output / convert output to python string
 import subprocess  
 result = subprocess.run(["dir"], shell=True, capture_output=True, text=True)
-print(result.stdout)
+# resuls -  captured output assigned to the variable 
+print(result.stdout)   # .stderr
+print(result)
+
+# 1.2. Same with   "cmd", "/c",   |accept shell=True
+import subprocess  
+result1 = subprocess.run(["cmd", "/c", "dir"], capture_output=True, text=True)
+print(result1.stdout)
+
+
+# 2. 
 
 # 3. Running a .bat file (Windows) or .sh file (Linux/macOS).
 import subprocess
