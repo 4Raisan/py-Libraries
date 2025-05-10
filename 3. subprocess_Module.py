73e110a6f77b67bf6f8c6fe758b72c5ced197d50
4Raisan <mn4raisan@gmail.com>
2025-05-10 22:12:27 +0530

    ping urls and open programs using subprocess

---
+++ 3. subprocess_Module.py
@@ -0,0 +1,10 @@
+'''  .subprocess()  - Run and manage, System commands (CMD/PowerShell) or External programs.  '''
+
+# Ping URLs
+import subprocess
+subprocess.run(["ping", "google.com"])
+
+# Open Programs (Softwares)
+import subprocess
+subprocess.run(["notepad"])
+
