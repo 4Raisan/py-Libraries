8b0678ec9f93edbb7e04f70d82ef78713a6b1fa5
4Raisan <mn4raisan@gmail.com>
2025-05-10 23:59:54 +0530

    usesubprocess with batchscript files and trminals

--- 3. Use_subprocess_Module.py
+++ 3. Use_subprocess_Module.py
@@ -3,6 +3,8 @@
 # List format preferred → subprocess.run(["echo", "Hello"])  → replace spaces with comma ( , )
 # Use raw strings (r"") for Windows paths
 #  shell=True  → only for Batch Script File Run or Command Run (for Raw - subprocess.run("echo %PATH%") )
+#  capture_output=True  → capture the output of the teminal
+#  text=True  → convert the above capture into Python String format
 
 # Ping URLs
 import subprocess
@@ -18,4 +20,16 @@ subprocess.run(["cmd", "/c", "dir /w"])  # CMD  → ( /w - just names )
 subprocess.run(["powershell", "ls"])  # PowerShell
 
 
+# Direct Run on Terminals and Get their Output
+
+# 1. Simple command with output
+# run in CMD shell / capture the output / convert output to python string
+import subprocess  
+result = subprocess.run(["dir"], shell=True, capture_output=True, text=True)
+print(result.stdout)
+
+# 3. Running a .bat file (Windows) or .sh file (Linux/macOS).
+import subprocess
+subprocess.run("mybatchs.bat", shell=True)
+ 
 
