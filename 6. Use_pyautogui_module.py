f5843d1b57878ded66980221b5ef93550cf6d016
4Raisan <mn4raisan@gmail.com>
2025-05-14 23:54:02 +0530

    click positions and drag

--- 6. Use_pyautogui_module.py
+++ 6. Use_pyautogui_module.py
@@ -5,12 +5,13 @@ pip install pyautogui '''
 pyautogui.write("Hello, world!", interval=0.1)   # interval, for each character 
 
 # Move the mouse to coordinates (100, 100)
-pyautogui.moveTo(100, 100, duration=1)
+pyautogui.moveTo(100, 100, duration=1)  # just appear on the location
+pyautogui.dragTo(400, 500)  # act like physical mouse going
 
 # Click the mouse -> default click on the mouse location 
 # -> specified location:  pyautogui.click(x=100, y=350)
 pyautogui.click()  # single click
-pyautogui.rightClick()  # right click
+pyautogui.rightClick(x=400, y=600)  # right click
 pyautogui.middleClick()  # middle click
 
 # Screenshot
